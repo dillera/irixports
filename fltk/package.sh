6c94c31c781440ad09c5137c5d08ce5a3a6a1390
Andrew Diller <dillera@gmail.com>
2019-07-21 08:08:18 -0700

    added fltk

---
+++ fltk/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=fltk
+version=1.3.1
+useconfigure=true
+files="https://www.fltk.org/pub/fltk/1.3.1/fltk-1.3.1-source.tar.gz fltk-1.3.1-source.tar.gz 717242e8aa118020cc05aa788015a2933895b99c"
